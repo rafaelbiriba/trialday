e56637c3ad0021feb67dc37818cf9117c9d3cbe0
Rafael Biriba <biribarj@gmail.com>
2016-11-06 15:47:57 -0200

    Creating test file for mock application

--- Gemfile
+++ Gemfile
@@ -1,3 +1,8 @@
+$: << 'lib'
 source 'https://rubygems.org'
 
 gem "rack"
+
+group :test do
+  gem "rack-test", require: "rack/test"
+end
---
+++ test/app_test.rb
@@ -0,0 +1,33 @@
+require 'test/unit'
+require 'rack/test'
+
+OUTER_APP = Rack::Builder.parse_file('config.ru').first
+
+class TestApp < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    OUTER_APP
+  end
+
+  test "should return 200 for GET /bla" do
+    get "/bla"
+    assert last_response.ok?
+  end
+
+  test "should return a json 1,2,3 for GET /bla" do
+    get "/bla"
+    assert_equal last_response.body, {"results"=> [1,2,3]}.to_json
+  end
+
+  test "should return 200 for POST /bla" do
+    post "/bla", name: "123"
+    assert last_response.ok?
+  end
+
+  test "should return the param name for POST /bla" do
+    response = { "name" => "123" }
+    post "/bla", response
+    assert_equal last_response.body, response.to_json
+  end
+end
